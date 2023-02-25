47b623c68b62239629c56051c5ff4075e0c177da
Роман Агеев <gegins1@mail.ru>
2023-02-25 17:13:33 +0300

    deploy

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось не с первого раза!')
 
 
 def second_page(request):
